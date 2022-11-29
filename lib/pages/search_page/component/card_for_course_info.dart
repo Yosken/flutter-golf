b87127bf2d8d5fdcf276dfc9f8a8de4ff42b20b1
Yosken <go.yosken.654@gmail.com>
2022-11-29 23:49:12 +0900

    Add Card Widget for search page

--- lib/pages/search_page/component/card_for_course_info.dart
+++ lib/pages/search_page/component/card_for_course_info.dart
@@ -1,24 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_golf/constant/importer_constant.dart';
 import 'package:flutter_golf/json/api_items.dart';
-
-
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 Widget makeCard(Result result) {
   final item = result.Item!;
-  return InkWell(
-    onTap: () {},
-    child: Padding(
-      padding: const EdgeInsets.only(bottom: 16),
+  return Padding(
+    padding: const EdgeInsets.only(bottom: 16),
+    child: InkWell(
+      onTap: () {},
       child: Card(
           margin: const EdgeInsets.symmetric(horizontal: 32),
-          elevation: 10,
+          elevation: 4,
           color: Colors.white,
           clipBehavior: Clip.antiAlias,
           shape: cardShape,
           child: Column(
             children: [
+              SizedBox(
+                height: 100,
+                width: 400,
+                child: Image(
+                  image: NetworkImage(item.golfCourseImageUrl!),
+                  fit: BoxFit.cover,
+                ),
+              ),
               Text(item.golfCourseName!),
+              Stack(
+                children: [
+                  Center(
+                    child: RatingBar.builder(
+                      //tapを無効
+                      ignoreGestures: true,
+                      itemSize: 48,
+                      initialRating: item.evaluation!,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                      itemBuilder: (context, _) => const Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      onRatingUpdate: (rating) {
+                      },
+                    ),
+                  ),
+                  Center(
+                      child: Text(
+                    item.evaluation.toString(),
+                    style: const TextStyle(
+                        fontSize: 24, height: 2, fontWeight: FontWeight.bold),
+                  )),
+                ],
+              )
             ],
           )),
     ),
--- lib/pages/search_page/page_search.dart
+++ lib/pages/search_page/page_search.dart
@@ -9,28 +9,32 @@ class SearchPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     final asyncApiData = ref.watch(searchCourseInfoFutureProvider);
 
     return Column(
       children: [
         searchPageTextField,
         asyncApiData.when(
-          data: (value){
-            // return ListView.builder(itemCount: value.hits,itemBuilder: (BuildContext context, int index){
-            //   final courseInfo = value.Items![index];
-            //   return makeCard(courseInfo);
-            // });
-            final results = value.Items;
-            final firstResult = results![0];
-            final courseInfo = firstResult.Item!;
-            return Text(courseInfo.golfCourseName!);
-          },
-          error: (error, stack) => Center(child: Text(error.toString()),),
-          loading: () {
-            return const CircularProgressIndicator();
-          }
-        ),
+            data: (value) {
+              final results = value.Items;
+              return Expanded(
+                child: ListView.builder(
+                    itemCount: value.hits,
+                    itemBuilder: (BuildContext context, int index) {
+                      final courseInfo = results![index];
+                      return makeCard(courseInfo);
+                    }),
+              );
+              // final firstResult = results![0];
+              // final courseInfo = firstResult.Item!;
+              // return Text(courseInfo.golfCourseName!);
+            },
+            error: (error, stack) => Center(
+                  child: Text(error.toString()),
+                ),
+            loading: () {
+              return const CircularProgressIndicator();
+            }),
       ],
     );
   }
